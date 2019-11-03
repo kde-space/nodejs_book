d2c919035815e1da8c84f93f5b1bed606da7ac31
koji.kobayashi <kojikobayashi1840@gmail.com>
2019-11-04 05:45:04 +0900

    contentTypes.js & utils.js 作成

---
+++ confetti_cuisine/contentTypes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const contentType = 'Content-Type';
+
+module.exports = {
+  html: {
+    [contentType]: 'text/html'
+  },
+  text: {
+    [contentType]: 'text/plain'
+  },
+  js: {
+    [contentType]: 'text/js'
+  },
+  css: {
+    [contentType]: 'text/css'
+  },
+  jpg: {
+    [contentType]: 'image/jpg'
+  },
+  png: {
+    [contentType]: 'image/png'
+  }
+};
