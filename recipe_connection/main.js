6c5c7c603c9001b06e01ac6369eff1223a3ad39f
koji.kobayashi <kojikobayashi1840@gmail.com>
2020-07-07 02:09:50 +0900

    reset

--- recipe_connection/main.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const cities = require('cities');
-let myCity = cities.zip_lookup('10016');
-console.log(myCity);
--- recipe_connection/printMessages.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const messageModule = require('./messages');
-messageModule.messages.forEach(message => console.log(message));
